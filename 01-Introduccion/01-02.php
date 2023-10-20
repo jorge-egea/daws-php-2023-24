bacf30d64833a1b89a733112214bee6b7e4cab2b
pirate <jorge.egea@ikasle.egibide.org>
2023-10-20 13:03:58 +0200

    Ejercicio 2

---
+++ 01-Introduccion/01-02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="author" content="Jorge Egea">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+<p>
+    02. Crea dos variables llamadas nombre y edad. La variable nombre tendrá el
+    valor “Mikel” y la variable edad tendrá asignado el valor 22. A continuación crea
+    una página que muestre la siguiente frase:
+</p>
+<?php
+$nombre = "Mikel";
+$edad = 22;
+echo "Mi amigo $nombre tiene $edad";
+?>
+</body>
+</html>
