8152f2ea0ec8389587d5bd5f074aecfc2ed8e4ba
pirate <jorge.egea@ikasle.egibide.org>
2023-10-20 12:59:07 +0200

    Ejercicio 01-01

---
+++ 01-Introduccion/01-01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="author" content="Jorge Egea">
+    <title>Ejercicio 01</title>
+</head>
+<body>
+<p>
+    01. Crea dos variables llamadas nombre y edad. La variable nombre tendrá el
+    valor “Mikel” y la variable edad tendrá asignado el valor 22. A continuación
+    muéstralas por pantalla.
+</p>
+<?php
+$nombre = "Mikel";
+$edad = 22;
+
+echo "<p>Nombre: $nombre</p>";
+echo "<p>Edad: $edad</p>"
+?>
+</body>
+</html>
