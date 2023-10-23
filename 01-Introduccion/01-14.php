4db2786da4588da90db6901ae1d97bb76a7fced7
TheUnknownSainth <TheUnknownSainth@outlook.com>
2023-10-23 20:25:03 +0200

    Ejercicio 14

---
+++ 01-Introduccion/01-14.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    14. Crea un array asociativo que incluya 5 palabras castellano y sus respectivas
+    traducciones al inglés. Muestra por pantalla las palabras y sus traducciones en
+    frases como esta:
+*/
+$palabras = ["Hola" => "Hello", "Mascota" => "Pet", "Perro" => "Dog", "Casa" => "House", "Mundo" => "World"];
+echo "<p>Hola: " . $palabras["Hola"] . "</p>";
+echo "<p>Mascota: " . $palabras["Mascota"] . "</p>";
+echo "<p>Perro: " . $palabras["Perro"] . "</p>";
+echo "<p>Casa: " . $palabras["Casa"] . "</p>";
+echo "<p>Mundo: " . $palabras["Mundo"] . "</p>";
